fe0ae6a28648a23daf3a034fc6c789536235dd9c
Will Belote <wbelote@unca.edu>
2019-11-25 13:03:09 -0500

    made timer entry work with text and timer

--- static/timer.js
+++ static/timer.js
@@ -7,7 +7,26 @@ var timer_display = document.getElementById("time-display");
 var timer_entry = document.getElementById("time-entry");
 timer_display.innerHTML = "ready";
 
-document.addEventListener("keyup", function (event) {
+document.addEventListener("keydown", (event) => {
+
+    if (timer_running) {
+        end_time = new Date();
+        diff = (end_time - start_time) / 1000;
+        timer_display.innerHTML = diff;
+        timer_entry.value = diff;
+        timer_running = false;
+    } else if (event.code == "Space") {
+        timer_display.style.color = "#33EE33";
+        diff = 0.0;
+        timer_display.innerHTML = diff;
+    }
+
+    timer_entry.value = timer_entry.value.trim();
+
+});
+
+document.addEventListener("keyup", (event) => {
+
     timer_display.style.color = "#FFFFFF";
 
     if (diff < 0.1) {
@@ -15,18 +34,15 @@ document.addEventListener("keyup", function (event) {
         timer_running = true;
         timer_display.innerHTML = diff;
     }
+
+    if (event.code == "Space") {
+        timer_entry.value = timer_entry.value.trim();
+    }
+
 });
 
-document.addEventListener("keydown", function (event) {
-    timer_display.style.color = "#33EE33";
-    if (timer_running) {
-        end_time = new Date();
-        diff = (end_time - start_time) / 1000;
-        timer_display.innerHTML = diff;
-        timer_entry.value = diff;
-    }
-    timer_running = false;
-});
+timer_entry.addEventListener("onchange", function (event) {
+})
 
 /*
 
