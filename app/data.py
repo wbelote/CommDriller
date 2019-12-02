58a1d2a14aba721221f052ef2553c55472dff8bb
Will Belote <wbelote@unca.edu>
2019-12-01 21:38:33 -0500

    changed cookie to session

--- app/data.py
+++ app/data.py
@@ -1,4 +1,5 @@
 import random
+import string
 
 import pandas as pd
 
@@ -19,7 +20,14 @@ def case_name(row):
 cases = pd.read_csv('comms.tsv', sep='\t', header=0)
 cases['repr'] = cases.apply(case_name, axis=1)
 
-times = pd.DataFrame(columns=['time', 'date', 'case_id'])
+users = pd.DataFrame(columns=['key'])
+times = pd.DataFrame(columns=['user_id', 'time', 'date', 'case_id'])
+
+
+def new_session():
+    sid = "".join(random.choices(string.ascii_lowercase + string.digits, k=50))
+    users.append({'key': sid}, ignore_index=True)
+    return sid
 
 
 def next_case():
@@ -33,8 +41,8 @@ def case_for_id(case_id):
     return c
 
 
-def history():
-    h = times.copy().sort_values(by='date', ascending=False)
+def history(user_id):
+    h = times.copy()[times['user_id'] == user_id].sort_values(by='date', ascending=False)
     names = cases.loc[list(h['case_id']), 'repr']
     h['case_name'] = list(names)
     return h
@@ -42,6 +50,7 @@ def history():
 
 def submit(form_data):
     form_data['time'] = float(form_data['time'])
+    form_data['user_id'] = users.index[users['key'] == form_data['user_id']]
     global times
     times = times.append(form_data, ignore_index=True)
     times['date'] = times['date'].astype(int)
--- app/routes.py
+++ app/routes.py
@@ -18,7 +18,13 @@ def corners():
 @app.route("/corners/<case_id>")
 def corners_id(case_id):
     case = data.case_for_id(case_id)
-    return render_template("main.html", case=case, history=data.history(), stats=[])
+    cookie = request.cookies.get('user_id')
+    if not cookie:
+        cookie = data.new_session()
+        res = make_response(render_template("main.html", case=case, history=data.history(cookie), stats=[]))
+        res.set_cookie('user_id', cookie)
+        return res
+    return render_template("main.html", case=case, history=data.history(cookie), stats=[])
 
 
 @app.route("/submit", methods=["POST", "GET"])
@@ -29,16 +35,15 @@ def submit():
         if not form_data["time"].strip():
             print("DATA NOT SUBMITTED")
             return redirect(f"corners/{form_data['case_id']}")
-        form_data['date'] = int(time.time())
 
-        # data.submit(form_data)
-        res = make_response(redirect(f"corners/{form_data['case_id']}"))
-        cookie = request.cookies.get('times')
-        res.set_cookie(
-            'times',
-            f"{cookie}i{round(float(form_data['time']), 3)}i{int(form_data['date'])}i{int(form_data['case_id'])}n"
-        )
-        return res
+        form_data['date'] = int(time.time())
+        cookie = request.cookies.get('user_id')
+        if not cookie:
+            return redirect(f"corners/{form_data['case_id']}")
+        form_data['user_id'] = cookie
+
+        data.submit(form_data)
+        return redirect(f"corners/{form_data['case_id']}")
 
     return redirect(url_for("corners"))
 
