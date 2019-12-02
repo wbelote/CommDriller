69d74cd80ea4be8ca662defa449a9433293146ad
Will Belote <wbelote@unca.edu>
2019-12-01 20:10:52 -0500

    added case to history list

--- app/data.py
+++ app/data.py
@@ -3,16 +3,24 @@ import random
 import pandas as pd
 
 piece_types = ["corners", ]
-stickers = pd.DataFrame([
-    {'type': 0, 'faces': s, 'letter': c, }
-    for s, c in zip(
-        'UBL URB UFR ULF LUB LFU LDF LBD FUL FRU FDR FLD RUF RBU RDB RFD BUR BLU BDL BRD DFL DRF DBR DLB'.split(),
-        'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-    )
-])
+stickers = pd.DataFrame({
+    'type': [0 for _ in range(24)],
+    'faces': 'UBL URB UFR ULF LUB LFU LDF LBD FUL FRU FDR FLD RUF RBU RDB RFD BUR BLU BDL BRD DFL DRF DBR DLB'.split(),
+    'letter': list('ABCDEFGHIJKLMNOPQRSTUVWX')
+})
+
+
+def case_name(row):
+    t1 = stickers.iloc[row['target1']]
+    t2 = stickers.iloc[row['target2']]
+    return f"UFR-{t1['faces']}-{t2['faces']} ({t1['letter']}{t2['letter']})"
+
 
 cases = pd.read_csv('comms.tsv', sep='\t', header=0)
-times = pd.DataFrame(columns=['time', 'date', 'case_id'])
+cases['repr'] = cases.apply(case_name, axis=1)
+print(cases)
+
+times = pd.DataFrame(columns=['time', 'date', 'case_id'], dtype=float)
 
 
 def next_case(pt=0):
@@ -22,21 +30,23 @@ def next_case(pt=0):
 def case_for_id(case_id):
     c = dict(cases.iloc[int(case_id)])
     c['id'] = case_id
-    t1 = stickers.iloc[c['target1']]
-    t2 = stickers.iloc[c['target2']]
-    c['name'] = f"UFR-{t1['faces']}-{t2['faces']} ({t1['letter']}{t2['letter']})"
+    c['name'] = case_name(c)
     return c
 
 
 def history():
-    return times
+    h = times.copy().sort_values(by='date')
+    h['case_name'] = cases.loc[list(h['case_id']), 'repr']
+    print("history:", h)
+    return h
 
 
 def submit(form_data):
     form_data['time'] = float(form_data['time'])
     global times
     times = times.append(form_data, ignore_index=True)
-    print(times)
+    times['date'] = times['date'].astype(int)
+    times['case_id'] = times['case_id'].astype(int)
 
 
 if __name__ == '__main__':
